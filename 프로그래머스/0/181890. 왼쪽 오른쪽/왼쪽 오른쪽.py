c3814ed26f02624798a9b76a79ea420db59e28b3
ahra Ko <79412889+ahra1221@users.noreply.github.com>
2025-07-28 10:56:59 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -0,0 +1,8 @@
+def solution(str_list):
+    answer = []
+    for i in range(len(str_list)):
+        if str_list[i] == "l":
+            return str_list[:i]
+        elif str_list[i] == "r":
+            return str_list[i+1:]
+    return answer
