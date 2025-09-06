54b8660fd93e7aebf3024bea027fee9004786fc4
ahra Ko <79412889+ahra1221@users.noreply.github.com>
2025-09-06 17:08:04 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,17 +1,25 @@
 import sys
+import sys
 
 input = sys.stdin.readline
+N = int(input())
+ans = 0
 
-n = int(input())
-cnt = n
+def check_group(word):
+    g = {word[0]}
+    for i in range(1, len(word)):
+        if word[i] in g:
+            if word[i-1] == word[i]: ## 연속중
+                continue 
+            else:
+                return False
+        else:
+            g.add(word[i])
+    return True
 
-for _ in range(n):
-    word = input().strip()
-    for i in range(len(word)-1):
-        if word[i] == word[i+1]:
-            pass
-        elif word[i] in word[i+1:]:
-            cnt -= 1
-            break
+for _ in range(N):
+    str = input().strip()
+    if check_group(str):
+        ans += 1
 
-print(cnt)
+print(ans)
