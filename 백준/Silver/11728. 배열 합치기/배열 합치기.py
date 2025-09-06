7f28175d18b18571e688ac8bde9a43cb803a1b76
ahra Ko <79412889+ahra1221@users.noreply.github.com>
2025-09-06 18:43:12 +0900

    [Silver V] Title: 배열 합치기, Time: 1832 ms, Memory: 186972 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+ans = []
+i, j = 0, 0
+
+while i<N or j<M:
+    if i>=N or (j<M and A[i]>B[j]):
+        ans.append(B[j])
+        j += 1
+    else:
+        ans.append(A[i])
+        i += 1
+print(*ans)
