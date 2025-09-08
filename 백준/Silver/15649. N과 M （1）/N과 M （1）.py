56d6c61495fe2c7ff353d350397131bf349516d4
ahra Ko <79412889+ahra1221@users.noreply.github.com>
2025-09-08 15:16:41 +0900

    [Silver III] Title: N과 M (1), Time: 144 ms, Memory: 37528 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,19 +1,22 @@
 import sys
+N, M = map(int, input().split())
 
-input = sys.stdin.readline
-
-
-def backtracking():
-    if len(arr) == m:
-        print(" ".join(map(str, arr)))
+def backtrack(n, m, used, tmp, ans):
+    if len(tmp) == m:
+        ans.append(tmp.copy())
         return
-    for i in range(1, n + 1):
-        if i not in arr:
-            arr.append(i)
-            backtracking()
-            arr.pop()
+    for i in range(1,n+1):
+        if used[i]:
+            continue
+        tmp.append(i)
+        used[i] = True
+        backtrack(n, m, used, tmp, ans)
+        tmp.pop()
+        used[i] = False
 
-
-n, m = map(int, input().split())
-arr = []
-backtracking()
+used = [False] * (N+1)
+ans = []
+tmp = []
+backtrack(N, M, used, tmp, ans)
+for a in ans:
+    print(" ".join(map(str, a)))
