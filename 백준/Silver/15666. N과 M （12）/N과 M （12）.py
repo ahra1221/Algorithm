c204046e84435d34d41076568be375ee33c07e78
ahra Ko <79412889+ahra1221@users.noreply.github.com>
2025-09-08 16:36:36 +0900

    [Silver II] Title: N과 M (12), Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+
+def backtrace(m, tmp, ans, st):
+    if len(tmp) == m:
+        ans.append(tmp.copy())
+        return
+    prev = None
+    for idx, i in enumerate(nums):
+        if i < st:
+            continue
+        if i == prev:
+            continue
+        tmp.append(i)
+        used[idx] = True
+        st = i
+
+        backtrace(m, tmp, ans, st)
+
+        tmp.pop()
+        used[idx] = False
+        prev = i
+
+used = [False] *(N+1)
+tmp, ans = [], []
+backtrace(M, tmp, ans, 1)
+
+for a in ans:
+    print(" ".join(map(str, a)))
