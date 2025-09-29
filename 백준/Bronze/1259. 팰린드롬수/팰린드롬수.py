0413dfa3a42d20e4077045d5760323c26e3dda8e
arako <79412889+ahra1221@users.noreply.github.com>
2025-09-29 17:33:49 +0900

    [Bronze I] Title: 팰린드롬수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,15 +1,8 @@
 import sys
-
 input = sys.stdin.readline
 
 while True:
-    num = input().strip()
-    if num == '0':
+    s = input().strip()
+    if s == "0":
         break
-    else:
-        reverse_num = num[::-1]
-        if reverse_num == num:
-            print("yes")
-        else:
-            print("no")
-
+    print("yes" if s == s[::-1] else "no")
