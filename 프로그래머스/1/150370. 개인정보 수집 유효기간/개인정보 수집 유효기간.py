3ed93fd438b43837c9a1831cf91269948a7047dc
arako <79412889+ahra1221@users.noreply.github.com>
2025-10-06 17:55:40 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.37 ms, Memory: 9.48 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,20 +1,26 @@
-def timeToDay(day):
-    y, m, d = map(int, day.split('.'))
-    return y * 12 * 28 + m * 28 + d
+def cal_expire(start, month):
+    y,m,d = map(int, start.split("."))
+    m += month
+    d -= 1
+    if d < 1:
+        m -= 1
+        d = 28
+    if m > 12:
+        y += (m-1) // 12
+        m = 12 if m % 12 == 0 else m % 12
+    elif m < 1:
+        y -= 1
+        m = 12
+    return f"{y}.{m:02}.{d:02}"
 
+def is_expire(today, ex):
+    return tuple(map(int, today.split("."))) > tuple(map(int, ex.split(".")))
 
 def solution(today, terms, privacies):
     answer = []
-    term = {}
-    today = timeToDay(today)
-
-    for t in terms:
-        x, y = t.split()
-        term[x] = int(y)
-
-    for idx, p in enumerate(privacies):
-        start_day, kind = p.split()
-        if timeToDay(start_day) + term[kind] * 28 <= today:
+    term = {k: int(v) for k, v in (t.split() for t in terms)}
+    for idx, pri in enumerate(privacies):
+        d, k = pri.split()
+        if is_expire(today, cal_expire(d, term[k])):
             answer.append(idx+1)
-        
     return answer
