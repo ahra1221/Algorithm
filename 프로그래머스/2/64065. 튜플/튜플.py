dd90a4fc85b419f10a29190dceda164ef33d590d
arako <79412889+ahra1221@users.noreply.github.com>
2025-10-10 09:35:15 +0900

    [level 2] Title: 튜플, Time: 45.14 ms, Memory: 22.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,14 +1,4 @@
-import re
-
 def solution(s):
     answer = []
-    matches = re.findall(r'\{(.*?)\}', s[1:-1])
-    match_list = [set(map(int, m.split(','))) for m in matches]
-    match_list = sorted(match_list, key=len)
-    for i in range(len(match_list)):
-        if i > 0:
-            value = next(iter(match_list[i] - match_list[i-1]))
-        else:
-            value = next(iter(match_list[i]))
-        answer.append(value)
+    print(s[1:-1].split("},"))
     return answer
