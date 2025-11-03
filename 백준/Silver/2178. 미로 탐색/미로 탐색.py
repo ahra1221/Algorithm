e71a4b68c4ec6015505c9e8d25bfc82d7aa4c9d6
arako <79412889+ahra1221@users.noreply.github.com>
2025-11-03 13:27:16 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 35084 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,31 +1,24 @@
+import sys
 from collections import deque
 
-n, m = map(int, input().split())
-graph = []
+input = sys.stdin.readline
+n,m = map(int, input().split())
+miro = [list(map(int, input().strip())) for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
+dirs = [(1,0),(-1,0),(0,1),(0,-1)]
+distance = [[0]*m for _ in range(n)]
 
-for _ in range(n):
-    graph.append(list(map(int, input().rstrip())))
+def bfs(r,c):
+    q = deque([(r,c)])
+    visited[r][c] = True
+    while q:
+        (nr, nc) = q.popleft()
+        for dx, dy in dirs:
+            x,y = nr+dx, nc+dy
+            if 0<=x<n and 0<=y<m and miro[x][y] == 1 and not visited[x][y]:
+                distance[x][y] = distance[nr][nc]+1
+                q.append((x,y))
+                visited[x][y] = True
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
-
-    while queue:
-        x, y = queue.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
-                queue.append((nx, ny))
-                graph[nx][ny] = graph[x][y] + 1
-
-    return graph[n - 1][m - 1]
-
-
-print(bfs(0, 0))
+bfs(0,0)
+print(distance[n-1][m-1]+1)
