6abbc6ca2478452bd32885b767c70eac1892f95e
arako <79412889+ahra1221@users.noreply.github.com>
2026-04-03 09:31:28 +0900

    Time: 257 ms (33.62%), Space: 21.5 MB (80.97%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,31 @@
 from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        r = len(grid)
-        c = len(grid[0])
-        v = [[False] * c for _ in range(r)]
-
-        def bfs(sr, sc, visited):
-            q = deque([(sr,sc)])
-            visited[sr][sc] = True
-            dirs = [(1,0),(-1,0),(0,1),(0,-1)]
-            while q:
-                row, col = q.popleft()
-                for dr, dc in dirs:
-                    nr, nc = row + dr, col + dc
-                    if 0<=nr<r and 0<=nc<c and not visited[nr][nc] and grid[nr][nc] == "1":
-                        q.append((nr,nc))
-                        visited[nr][nc] = True
         ans = 0
-        for i in range(r):
-            for j in range(c):
-                if not v[i][j] and grid[i][j] == "1":
-                    bfs(i,j,v)
-                    ans += 1
+        m = len(grid)
+        n = len(grid[0])
+        visited = [[False] * n for _ in range(m)]
+        dx = [-1,1,0,0]
+        dy = [0,0,-1,1]
 
+        def bfs(x,y):
+            visited[x][y] = True
+            queue = deque()
+            queue.append((x,y))
+            while queue:
+                cur_x, cur_y = queue.popleft()
+                for l in range(4):
+                    next_x = cur_x + dx[l]
+                    next_y = cur_y + dy[l]
+                    if next_x >= 0 and next_x < m and next_y >= 0 and next_y < n:
+                        if grid[next_x][next_y] == "1" and not visited[next_x][next_y]:
+                            visited[next_x][next_y] = True
+                            queue.append((next_x, next_y))
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1" and not visited[i][j]:
+                    bfs(i,j)
+                    ans += 1
         return ans
