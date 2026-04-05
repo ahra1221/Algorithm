a324e8f935c56c5ef3fc0dd4a4e508b1758d27d2
arako <79412889+ahra1221@users.noreply.github.com>
2026-04-05 12:44:59 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/README.md
+++ 프로그래머스/0/181905. 문자열 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 뒤집기 - 181905 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181905?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181905) 
 
 ### 성능 요약
 
-메모리: 4.15 MB, 시간: 0.02 ms
+메모리: 4.14 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 05일 12:38:56
+2026년 04월 05일 12:44:56
 
 ### 문제 설명
 
--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cpp
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cpp
@@ -1,15 +1,11 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 string solution(string my_string, int s, int e) {
-    string answer = "";
-    answer += my_string.substr(0,s);
-    for(int i=e; i>=s; i--){
-        answer += my_string[i];
-    }
-    answer += my_string.substr(e+1,my_string.length()-e);
-    return answer;
+    reverse(my_string.begin() + s, my_string.begin() + e + 1);
+    return my_string;
 }
