060a0eb8d3350bc479c08794f08ceb90ba863195
arako <79412889+ahra1221@users.noreply.github.com>
2026-04-06 19:04:18 +0900

    [Silver II] Title: 나무 자르기, Time: 4864 ms, Memory: 148224 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,22 +1,24 @@
-# 코드를 작성해주세요
-import sys 
+import sys
 
-N, M = map(int, sys.stdin.readline().split())
-trees = list(map(int, sys.stdin.readline().split()))
+input = sys.stdin.readline
+N,M = map(int, input().split())
+trees = list(map(int, input().split()))
 
-left, right = 0, max(trees)
-ans = max(trees)
+st = 0
+end = max(trees)
+ans = 0
 
-while left <= right:
-    h = 0
-    mid = (left+right+1) // 2
-    for i in trees:
-        if i >= mid:
-            h += (i - mid)
+while st <= end:
+    mid = (st + end) // 2
+    total = 0
+    for tree in trees:
+        if tree > mid:
+            total += tree - mid
     
-    if h >= M:
+    if total >= M:
         ans = mid
-        left = mid + 1
+        st = mid + 1
     else:
-        right = mid - 1
+        end = mid - 1
+
 print(ans)
