275258f705a12c8939c38808d605cef4d50436b3
arako <79412889+ahra1221@users.noreply.github.com>
2026-04-07 15:09:26 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.cpp
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -2,19 +2,23 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <iostream>
 #include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> d, int budget) {
     int answer = 0;
-    int sum = 0;
-    
     sort(d.begin(), d.end());
-    for(auto i: d) {
-        if(sum + i > budget) break;
-        sum += i;
-        answer ++;
+    int total = 0;
+    for (auto x: d) {
+        total += x;
+        if (total <= budget) {
+            answer ++;
+            continue;
+        } else {
+            return answer;
+        }
     }
     return answer;
 }
