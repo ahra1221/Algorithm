e14c6ac3bafc21fed80b3bd874bdf4deaa80d2ba
arako <79412889+ahra1221@users.noreply.github.com>
2026-04-11 20:03:34 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 6.50 ms, Memory: 7.22 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.cpp
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.cpp
@@ -1,75 +1,68 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
     vector<int> answer;
-    int dir = 0; // 0 right, 1 down, 2 left, 3 up
-    vector<pair<int,int>> dirs = {
-        {0,1},
-        {1,0},
-        {0,-1},
-        {-1,0}
-    };
-    vector<vector<int>> v(rows, vector<int>(columns));
+    
+    vector<vector<int>> graph(rows, vector<int>(columns,0));
     int num = 1;
-    for(int i = 0; i < rows; i++) {
-        for (int j=0; j<columns;j++) {
-            v[i][j] = num++;
+    for(int i=0;i<rows;i++) {
+        for(int j=0;j<columns;j++) {
+            graph[i][j] = num++;
         }
     }
     
-    for (auto& q : queries) {
-        int x1 = q[0];
-        int y1 = q[1];
-        int x2 = q[2];
-        int y2 = q[3];
+    for(auto q: queries) {
+        int x1 = q[0]-1, y1 = q[1]-1;
+        int x2 = q[2]-1, y2 = q[3]-1;
         
-        vector<pair<int,int>> border;
-        int nx = x1;
-        int ny = y1;
-        
+        int nx = x1, ny = y1;
+        int min_val = graph[x2][y2];
+        int prev = graph[nx][ny];
         // right
-        while (ny < y2) {
-            border.push_back({nx,ny});
-            ny ++;
+        while(ny <= y2) {
+            if (ny == y2) break;
+            min_val = min(min_val, prev);
+            int next = graph[x1][ny+1];
+            graph[x1][ny+1] = prev;
+            prev = next;
+            ny++;
         }
         
         // down
-        while (nx < x2) {
-            border.push_back({nx,ny});
+        while(nx <= x2) {
+            if (nx == x2) break;
+            min_val = min(min_val, prev);
+            int next = graph[nx+1][y2];
+            graph[nx+1][y2] = prev;
+            prev = next;
             nx++;
         }
         
         // left
-        while (ny > y1) {
-            border.push_back({nx,ny});
+        while(ny >= y1) {
+            if (ny == y1) break;
+            min_val = min(min_val, prev);
+            int next = graph[x2][ny-1];
+            graph[x2][ny-1] = prev;
+            prev = next;
             ny--;
         }
         
         // up
-        while (nx > x1) {
-            border.push_back({nx,ny});
+        while(nx >= x1) {
+            if(nx==x1) break;
+            min_val = min(min_val, prev);
+            int next = graph[nx-1][y1];
+            graph[nx-1][y1] = prev;
+            prev = next;
             nx--;
         }
-        
-        int x = border[0].first;
-        int y = border[0].second;
-        int tmp = v[x-1][y-1];
-        int min_num = tmp;
-        
-        for(int i=1;i<border.size();i++) {
-           auto [nextx,nexty] = border[i];
-           int next = v[nextx-1][nexty-1];
-           v[nextx-1][nexty-1] = tmp;
-           tmp = next;
-           min_num = min(min_num, tmp);
-        }
-        v[x-1][y-1] = tmp;
-        
-        answer.push_back(min_num);
+        answer.push_back(min_val);
     }
     return answer;
 }
