148e327e6c2297961f3b947e01fd7b2efda4378d
arako <79412889+ahra1221@users.noreply.github.com>
2026-04-12 10:54:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.29 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 4.13 MB, 시간: 0.27 ms
+메모리: 4.13 MB, 시간: 0.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 06일 09:34:08
+2026년 04월 12일 10:54:17
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,36 +1,39 @@
 #include<vector>
-#include<queue>
+#include <queue>
+#include <tuple>
 using namespace std;
 
 int solution(vector<vector<int> > maps)
 {
+    int answer = -1;
     int n = maps.size();
     int m = maps[0].size();
     
-    vector<vector<int>> dist(n, vector<int>(m,-1));
-    int dirx[4] = {-1,1,0,0};
-    int diry[4] = {0,0,-1,1};
+    int dirx[4] = {1,-1,0,0};
+    int diry[4] = {0,0,1,-1};
+    vector<vector<bool>> visited(n, vector<bool>(m, false));
     
-    // bfs
-    queue<pair<int,int>> q;
-    q.push({0,0});
-    dist[0][0] = 1;
-    
-    while (!q.empty()) {
-        auto [x,y] = q.front();
+    queue<tuple<int,int,int>> q;
+    q.push({0,0,0});
+    visited[0][0] = true;
+    while(!q.empty()) {
+        auto [curx,cury,curd] = q.front();
         q.pop();
-        
-        for(int i=0; i<4; i++) {
-            int nx = x + dirx[i];
-            int ny = y + diry[i];
-            if (0<=nx && nx<n && 0<=ny && ny<m) {
-                if (maps[nx][ny] == 1 && dist[nx][ny] < 0) {
-                    dist[nx][ny] = dist[x][y] + 1;
-                    q.push({nx,ny});
+        if (curx == n-1 && cury == m-1) {
+            answer = curd + 1;
+            break;
+        }
+        for(int i=0;i<4;i++) {
+            int nextx = curx + dirx[i];
+            int nexty = cury + diry[i];
+            if(0<=nextx && nextx<n && 0<=nexty && nexty<m) {
+                if(maps[nextx][nexty] > 0 && !visited[nextx][nexty]) {
+                    q.push({nextx,nexty,curd+1});
+                    visited[nextx][nexty] = true;
                 }
             }
         }
     }
     
-    return dist[n-1][m-1];
+    return answer;
 }
