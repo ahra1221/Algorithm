7f15a0124dc328d7954f87d6637af57746f4397c
arako <79412889+ahra1221@users.noreply.github.com>
2026-05-03 16:17:57 +0900

    [level 2] Title: 가장 큰 수, Time: 156.86 ms, Memory: 21.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=swift) 
 
 ### 성능 요약
 
-메모리: 23 MB, 시간: 391.02 ms
+메모리: 21.6 MB, 시간: 156.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 28일 19:25:50
+2026년 05월 03일 16:17:54
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
@@ -1,8 +1,10 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> String {
-    let arr = numbers.map{String($0)}
-    let sorted_arr = arr.sorted {$0 + $1 > $1 + $0}
-    if sorted_arr[0] == "0" { return "0" }
-    return sorted_arr.joined()
+    var stringNum = numbers.map {String($0)}
+    stringNum.sort {
+        return $0 + $1 > $1 + $0
+    }
+    let answer = stringNum.joined()
+    return answer.first == "0" ? "0" : answer
 }
