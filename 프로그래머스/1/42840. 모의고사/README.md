dc4fad201f61f794de126a2449b83912df1adedf
arako <79412889+ahra1221@users.noreply.github.com>
2026-05-03 19:20:47 +0900

    [level 1] Title: 모의고사, Time: 2.40 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=swift) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.35 ms
+메모리: 16.9 MB, 시간: 2.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 05일 15:46:28
+2026년 05월 03일 19:20:42
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.swift
+++ 프로그래머스/1/42840. 모의고사/모의고사.swift
@@ -1,27 +1,22 @@
 import Foundation
 
 func solution(_ answers:[Int]) -> [Int] {
-    let people = [
-        [1, 2, 3, 4, 5],
-        [2, 1, 2, 3, 2, 4, 2, 5],
-        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    ]
-    var high = 0
-    var ans: [Int] = []
-    for (idx,p) in people.enumerated() {
-        var score = 0
-        for i in 0..<answers.count {
-            if answers[i] == p[i % p.count] {
-                score += 1
-            }
-        }
-        if score > high {
-            high = score
-            ans = [idx+1]
-        } else if score == high {
-            ans.append(idx+1)
-        }
+    let p1 = [1, 2, 3, 4, 5]
+    let p2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    let p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    var cnt = [0,0,0]
+    
+    for (idx,ans) in answers.enumerated() {
+        if p1[idx%p1.count] == ans { cnt[0] += 1 }
+        if p2[idx%p2.count] == ans { cnt[1] += 1 }
+        if p3[idx%p3.count] == ans { cnt[2] += 1 }
     }
     
-    return ans
+    let max = cnt.max()
+    var answer = [Int]()
+    for (idx,c) in cnt.enumerated() {
+        if c == max { answer.append(idx+1)}
+    }
+    
+    return answer.sorted()
 }
